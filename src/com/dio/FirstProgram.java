988cd0336fb2d429b75cae3ed2c8fdf164b6c48b
Gabriele Rocha <gabrielerocha17@outlook.com>
2022-07-11 13:00:52 -0300

    Teste sobre IDE Eclipse

---
+++ src/com/dio/FirstProgram.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import com.dio.modules.Gato;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato (null, null, null);
+		System.out.println(gato);
+		/*int a = 2;
+		int b = 4;
+		System.out.println("Hello Word : " + (a+b) );*/
+	} 
+}
